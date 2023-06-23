7d1796aa07f1a5b6149a8956418a4a84a56401a8
Venkat Bodempudi <bodempudi111@gmail.com>
2023-06-23 18:18:28 +0530

    added snakes

--- Projects/SnakeGame.py
+++ Projects/SnakeGame.py
@@ -1,2 +1,20 @@
 from turtle import Turtle,Screen
 
+#set up screen
+snakeScreen=Screen()
+snakeScreen.screensize(500,500)
+snakeScreen.bgcolor("black")
+snakeScreen.title("My Snake Game")
+
+Snake1=Turtle("square")
+Snake1.color("White")
+
+Snake2=Turtle("square")
+Snake2.color("White")
+Snake2.goto(-20,0)
+
+Snake3=Turtle("square")
+Snake3.color("White")
+Snake3.goto(-40,0)
+
+snakeScreen.exitonclick()
