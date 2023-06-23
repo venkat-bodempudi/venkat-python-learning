6a08fedc31c3ac5a9bcf6bafe54e293efc00bade
Venkat Bodempudi <bodempudi111@gmail.com>
2023-06-23 20:04:39 +0530

    SnakeGame development in progress

--- Projects/SnakeGame.py
+++ Projects/SnakeGame.py
@@ -1,20 +1,33 @@
 from turtle import Turtle,Screen
-
+import time
 #set up screen
 snakeScreen=Screen()
 snakeScreen.screensize(500,500)
 snakeScreen.bgcolor("black")
 snakeScreen.title("My Snake Game")
+snakeScreen.tracer(0)
+snakes=[]
 
-Snake1=Turtle("square")
-Snake1.color("White")
+initial_snake_positions=[(0,0),(-20,0),(-40,0)]
 
-Snake2=Turtle("square")
-Snake2.color("White")
-Snake2.goto(-20,0)
+for snake_position in initial_snake_positions:
+    snake=Turtle("square")
+    snake.color("White")
+    snake.penup()
+    snake.goto(snake_position)
+    snakes.append(snake)
+ 
+is_game_completed=True
+while is_game_completed:
+    snakeScreen.update()  
+    time.sleep(0.1)
 
-Snake3=Turtle("square")
-Snake3.color("White")
-Snake3.goto(-40,0)
+    for snake_index in range(len(snakes)-1,0,-1):
+        new_x=snakes[snake_index-1].xcor()
+        new_y=snakes[snake_index-1].ycor()
+        snakes[snake_index].goto(new_x,new_y)
+    snakes[0].forward(20)
+    
+
 
 snakeScreen.exitonclick()
